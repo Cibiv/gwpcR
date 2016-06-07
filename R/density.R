492f028cd33df890a71cbd48a6d76be9e4edc9a2
Florian G. Pflug <florian.pflug@univie.ac.at>
2016-06-07 01:24:28 +0000

    Fixed density.interpolate to actually compute data.akima.wk.
    Must pass non-zero number of points, even though the WK arrays
    are independent from the points we interpolate at.

--- R/density.R
+++ R/density.R
@@ -18,6 +18,7 @@ density.interpolate <- function(x0, y0, m) {
   if (is.null(GWPCR$data.akima.wk))
     GWPCR$data.akima.wk <- list()
   if ((m > length(GWPCR$data.akima.wk)) || is.null(GWPCR$data.akima.wk[[m]])) {
+    # These checks cannot fail, unless the data in sysdata.rda is tampared with
     #stopifnot(is.double(GWPCR$efficiency) && is.double(GWPCR$lambda))
     #stopifnot(is.double(GWPCR$data[[m]]))
     #stopifnot(length(GWPCR$efficiency) == nrow(GWPCR$data[[m]]))
@@ -26,22 +27,24 @@ density.interpolate <- function(x0, y0, m) {
                   md=as.integer(1),
                   nxd=length(GWPCR$efficiency), nyd=length(GWPCR$lambda),
                   xd=GWPCR$efficiency, yd=GWPCR$lambda, zd=GWPCR$data[[m]],
-                  nip=as.integer(0),
-                  xi=double(), yi=double(), zi=double(),
+                  nip=as.integer(1),
+                  xi=GWPCR$efficiency[1], yi=GWPCR$lambda[1], zi=double(1),
                   ier=integer(1),
                   wk=double(3*length(GWPCR$efficiency)*length(GWPCR$lambda)),
                   PACKAGE="akima", NAOK=TRUE)
+    stopifnot(r$ier == 0)
     GWPCR$data.akima.wk[[m]] <- r$wk
   }
 
+  # These checks cannot fail, unless the data in sysdata.rda is tampared with
   #stopifnot(is.double(GWPCR$efficiency) && is.double(GWPCR$lambda))
   #stopifnot(is.double(GWPCR$data[[m]]))
   #stopifnot(length(GWPCR$efficiency) == nrow(GWPCR$data[[m]]))
   #stopifnot(length(GWPCR$lambda) == ncol(GWPCR$data[[m]]))
   #stopifnot(is.double(GWPCR$data.akima.wk[[m]]))
   #stopifnot(length(GWPCR$data.akima.wk[[m]]) == 3*length(GWPCR$efficiency)*length(GWPCR$lambda))
-  #stopifnot(is.double(x0) && is.double(y0))
-  #stopifnot(length(x0) == length(y0))
+  stopifnot(is.double(x0) && is.double(y0))
+  stopifnot(length(x0) == length(y0))
   r <- .Fortran("rgbi3p",
                 md=as.integer(2),
                 nxd=length(GWPCR$efficiency), nyd=length(GWPCR$lambda),
@@ -49,7 +52,8 @@ density.interpolate <- function(x0, y0, m) {
                 nip=length(x0),
                 xi=x0, yi=y0, zi=double(length(x0)),
                 ier=integer(1),
-                wk=double(3*length(GWPCR$efficiency)*length(GWPCR$lambda)),
+                wk=GWPCR$data.akima.wk[[m]],
                 PACKAGE="akima", NAOK=TRUE)
+  stopifnot(r$ier == 0)
   return(pmax(r$zi, 0))
 }
