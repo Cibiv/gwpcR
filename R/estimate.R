b14b7e70bd7da79ef52e97da79f925cd17c6b723
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-01 22:32:03 +0200

    Added copyright notices

--- R/estimate.R
+++ R/estimate.R
@@ -1,3 +1,20 @@
+# estimate.R, Copyright 2016,2017 Florian G. Pflug
+#
+# This file is part of gwpcR
+#
+# Foobar is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
 #' Method-of-Moments Parameter Estimation for PCR-Poisson Mixture
 #'
 #' Estimates the parameters \var{efficiency} and \var{lambda0} from the sample
