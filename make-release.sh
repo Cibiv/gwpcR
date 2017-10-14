1edc6c7da98a1bba3a1bba9c5720b80c0d15eaf5
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-10-14 20:37:38 +0200

    Added make-release.sh

---
+++ make-release.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+ver=$1
+if [ "$ver" == "" ]; then
+	echo "Usage: $0 version" >&2
+	exit 1
+fi
+
+if [ "$(git symbolic-ref --short HEAD)" != "master" ]; then
+	echo "Currently checkout out branch must be 'master'" >&2
+	exit 1
+fi
+
+if [ $(git status --porcelain | wc -l) != 0 ]; then
+	echo "Working copy contaisn uncommitted changes" >&2
+	exit 1
+fi
+
+if [ $(git ls-remote --tags origin v$ver | wc -l) != 0 ]; then
+	echo "Version $ver already released (remote tag v$ver already exists)" >&2
+	exit 1
+fi
+
+echo "Updating DESCRIPTION" >&2
+sed -i .bak 's/^Version: \(.*\)$/Version: '"$ver"'/' DESCRIPTION
+rm DESCRIPTION.bak
+
+echo "Comitting change" >&2
+git add DESCRIPTION
+git commit -m "Incremented version to $ver"
+
+echo "Tagging as v$ver and latest" >&2
+git tag -f v$ver
+
+echo "Pushing to origin" >&2
+git push origin master v$ver
+
+echo "Updating 'latest' on origin" >&2
+# tag would be more logical than head, but R's install_git only works for branches
+git push -f origin v$ver:refs/heads/latest-release
