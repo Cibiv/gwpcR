b6cbe1e6484960cd906977d0fae3db4c4ed6311f
Florian G. Pflug <florian.pflug@univie.ac.at>
2017-12-21 19:53:52 +0100

    Increased test coverage

--- tests/testthat/test_gwpcr.R
+++ tests/testthat/test_gwpcr.R
@@ -6,7 +6,7 @@ P.TH <- 0.1
 TRIALS <- 6
 
 test_gwpcr <- function(efficiency, molecules, N) {
-  test_that(paste0("gwpcr(efficiency=", 100*efficiency, "%)"), {
+  test_that(paste0("rgwpcr, pgwpcr (efficiency=", 100*efficiency, "%)"), {
     i <- 0
     p <- 0
     while((p <= P.TH) && (i < TRIALS)) {
@@ -47,3 +47,28 @@ test_gwpcr(0.40, 5, 1e4)
 test_gwpcr(0.80, 5, 1e4)
 test_gwpcr(0.95, 5, 1e4)
 test_gwpcr(0.99, 5, 1e4)
+
+test_that("gwpcr (efficiency=0%, finite cycle count)", {
+  expect_identical(rgwpcr(n=5, efficiency=0, molecules=1, cycles=0), c(1,1,1,1,1))
+  expect_identical(rgwpcr(n=5, efficiency=0, molecules=1, cycles=1), c(1,1,1,1,1))
+  expect_identical(rgwpcr(n=5, efficiency=0, molecules=1, cycles=2), c(1,1,1,1,1))
+})
+
+test_that("gwpcr (efficiency=1e-6, finite cycle count)", {
+  # Will fail with probability =~ 5e-6.
+  expect_identical(rgwpcr(n=5, efficiency=1e-6, molecules=1, cycles=0), c(1,1,1,1,1))
+  expect_identical(rgwpcr(n=5, efficiency=1e-6, molecules=1, cycles=1), c(1,1,1,1,1)/(1+1e-6))
+  expect_identical(rgwpcr(n=5, efficiency=1e-6, molecules=1, cycles=2), c(1,1,1,1,1)/(1+1e-6)^2)
+})
+
+test_that("pgwpcr", {
+  # XXX: Shouldn't this raise an error instead?
+  expect_identical(pgwpcr(-1.0, efficiency=-1), as.numeric(NA))
+})
+
+test_that("pgwpcr.fun", {
+  f <- dgwpcr.fun(efficiency=0.5, molecules=1)
+  expect_equal(f(-0.5), 0.0)
+  expect_equal(f(0), 0.0)
+  expect_equal(f(max(gwpcR:::GWPCR$lambda)), 0.0)
+})
---
+++ tests/testthat/test_gwpcrpois.R
@@ -0,0 +1,55 @@
+library(gwpcR)
+library(data.table)
+context('Galton-Watson PCR (gwpcrpois) distribution functions')
+
+P.TH <- 0.1
+TRIALS <- 6
+
+test_gwpcrpois <- function(efficiency, lambda0, threshold, molecules, N) {
+  test_that(paste0("rgwpcrpois, pgwpcrpois (efficiency=", 100*efficiency,
+                   "%. lambda0=", lambda0 , ")"), {
+    i <- 0
+    p <- 0
+    while((p <= P.TH) && (i < TRIALS)) {
+      i <- i + 1
+      x <- rgwpcrpois(n=N, efficiency=efficiency, lambda0=lambda0, molecules=molecules, threshold=threshold)
+      x.uniq <- sort(unique(x))
+      cdf <- stepfun(x=c(x.uniq, x.uniq[length(x.uniq)]+1),
+                     y=c(0, pgwpcrpois(x.uniq, efficiency=efficiency, lambda0=lambda0,
+                                       threshold=threshold, molecules=molecules), 1.0))
+      # Must use K-S test from dgof, because the distribution is discrete!
+      p <- dgof::ks.test(x, cdf, exact=FALSE)$p.value
+    }
+    expect_gt(p, P.TH)
+  })
+}
+
+test_gwpcrpois(0.00, 5  , 1, 1, 1e4)
+test_gwpcrpois(0.01, 50 , 0, 1, 1e4)
+test_gwpcrpois(0.05, 500, 1, 1, 1e4)
+test_gwpcrpois(0.10, 4  , 0, 1, 1e4)
+test_gwpcrpois(0.20, 40 , 1, 1, 1e4)
+test_gwpcrpois(0.40, 400, 0, 1, 1e4)
+test_gwpcrpois(0.80, 3  , 1, 1, 1e4)
+test_gwpcrpois(0.95, 30 , 0, 1, 1e4)
+test_gwpcrpois(0.99, 300, 1, 1, 1e4)
+
+test_gwpcrpois(0.00, 5  , 0, 2, 1e4)
+test_gwpcrpois(0.01, 50 , 1, 2, 1e4)
+test_gwpcrpois(0.05, 500, 0, 2, 1e4)
+test_gwpcrpois(0.10, 4  , 1, 2, 1e4)
+test_gwpcrpois(0.20, 40 , 0, 2, 1e4)
+test_gwpcrpois(0.40, 400, 1, 2, 1e4)
+test_gwpcrpois(0.80, 3  , 0, 2, 1e4)
+test_gwpcrpois(0.95, 30 , 1, 2, 1e4)
+test_gwpcrpois(0.99, 300, 0, 2, 1e4)
+
+test_gwpcrpois(0.00, 5  , 2, 5, 1e4)
+test_gwpcrpois(0.01, 50 , 1, 5, 1e4)
+test_gwpcrpois(0.05, 500, 2, 5, 1e4)
+test_gwpcrpois(0.10, 4  , 1, 5, 1e4)
+test_gwpcrpois(0.20, 40 , 2, 5, 1e4)
+test_gwpcrpois(0.40, 400, 1, 5, 1e4)
+test_gwpcrpois(0.80, 3  , 2, 5, 1e4)
+test_gwpcrpois(0.95, 30 , 1, 5, 1e4)
+test_gwpcrpois(0.99, 300, 2, 5, 1e4)
