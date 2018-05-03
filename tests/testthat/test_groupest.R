396ac8ea9e9b55f5b1c960ac627fb9fa8554fe34
Florian G. Pflug <florian.pflug@univie.ac.at>
2018-05-03 17:13:59 +0200

    Test gwpcrpois.groupest with multi-column group keys.

--- tests/testthat/test_groupest.R
+++ tests/testthat/test_groupest.R
@@ -40,9 +40,53 @@ test_est_grp <- function(efficiency, efficiency.sd, lambda0, lambda0.sd) {
   })
 }
 
+test_est_grp_mkey <- function() {
+  test_that("group estimation with multi-field group key", {
+    sample <- as.factor(intToUtf8(65:68, multiple=TRUE))
+    efficiency <- seq(from=0, to=1, by=0.05)
+    lambda0 <- c(1,5,10)
+    n.obs <- c(10000, 20000)
+
+    # Input parameters
+    g.in <- data.table(sample=rep(sample, each=length(efficiency)*length(lambda0)),
+                       efficiency.true=rep(efficiency, each=length(lambda0), length(sample)),
+                       lambda0.true=rep(lambda0, length(efficiency)*length(sample)),
+                       n.obs=n.obs)
+    setkey(g.in, sample, efficiency.true, lambda0.true)
+
+    # Simulate observations
+    g.obs <- g.in[, list(count=rgwpcrpois(n=n.obs, threshold=0, molecules=1,
+                                          efficiency=efficiency.true,
+                                          lambda0=lambda0.true))
+                 , by=c("sample", "efficiency.true", "lambda0.true")]
+
+    # Groupwise estimation
+    g.est <- withCallingHandlers({
+      gwpcrpois.groupest(count ~ sample + efficiency.true + lambda0.true, g.obs, threshold=0, molecules=1)
+    }, warning=function(e) {
+      expected = c("estimator variance of efficiency within groups estimated to be negative, setting to zero",
+                   "estimator variance of lambda0 within groups estimated to be negative, setting to zero")
+      if (!(conditionMessage(e) %in% expected))
+        warning("unexpected warning: ", conditionMessage(e))
+      invokeRestart("muffleWarning")
+    })
+
+    # Check that the output table contains the same groups
+    expect_equivalent(g.est$sample, g.in$sample)
+    expect_equivalent(g.est$efficiency.true, g.in$efficiency.true)
+    expect_equivalent(g.est$lambda0.true, g.in$lambda0.true)
+
+    # Check that the between-group standard deviations of efficiency and lambda0 are approximately recovered
+    expect_lt(g.est[, max(abs(efficiency - efficiency.true))], EPS.ABS.EFFICIENCY)
+    expect_lt(g.est[, max(abs(lambda0 - lambda0.true) / lambda0)], ESP.REL.LAMBDA0)
+  })
+}
+
 test_est_grp(0.2, 0.05,  5, 1)
 test_est_grp(0.2, 0.05, 15, 3)
 test_est_grp(0.5, 0.1 ,  5, 1)
 test_est_grp(0.5, 0.1 , 15, 3)
 test_est_grp(0.8, 0.1 ,  5, 1)
 test_est_grp(0.8, 0.1 , 15, 3)
+
+test_est_grp_mkey()
